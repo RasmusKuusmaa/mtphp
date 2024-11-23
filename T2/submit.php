825a92317c2c5b48e21bfa4c1d98420fc7632694
RasmusKuusmaa <77f315@gmail.com>
2024-11-24 00:03:42 +0200

    submit data to db

--- T2/submit.php
+++ T2/submit.php
@@ -1,6 +1,9 @@
 <?php
-
+include 'db.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
     $text = $_POST['text'];
+    $stmt = $pdo->prepare("insert into text (data) values (:text_content)");
+    $stmt->execute(['text_content' => $text]);
+    echo "text submitted";
     
 }

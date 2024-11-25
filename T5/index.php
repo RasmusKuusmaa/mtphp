b0909bae10d6695c2812332b8835f309657e92cc
RasmusKuusmaa <77f315@gmail.com>
2024-11-25 19:34:57 +0200

    show the fixed text to the user

--- T5/index.php
+++ T5/index.php
@@ -6,13 +6,18 @@
     <title>Koma vigade parandaja</title>
 </head>
 <body>
-    <form action="veaparandaja.php" method="post"> 
+    <form action="veaparandaja.php" method="POST"> 
         <label>Sisesta tekst</label>
         <textarea  name="tekst"></textarea>
         <button type="submit">Paranda vead</button>
     </form>
     <label>Parandatud tekst:</label>
-    <div id="parandatud_tekst">
-    </div>
+    <textarea id="parandatud_tekst"  readonly>
+        <?php
+            if (isset($_GET['parandatud_tekst'])){
+                echo ($_GET['parandatud_tekst']);
+            }
+        ?>
+    </textarea>
 </body>
 </html>
--- T5/veaparandaja.php
+++ T5/veaparandaja.php
@@ -1,6 +1,9 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === "POST"){
-    echo $_POST['tekst'];
+    $parandatud_tekst = $_POST['tekst'];
+    $parandatud_tekst .= " > Parandatud";
     
+    header("Location: index.php?parandatud_tekst=" . urlencode($parandatud_tekst));
+    exit();
 }
